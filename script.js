3a0203e298a501b474d93b9b2c859199113736cc
Deyvisson <68565306+Deyvi-dev@users.noreply.github.com>
2022-09-04 01:26:48 -0300

    fist comit

---
+++ script.js
@@ -0,0 +1,45 @@
+const inputElement = document.querySelector("input");
+const buttonElement = document.querySelector("form button");
+const ulElement = document.querySelector("ul");
+
+buttonElement.onclick = ev => {
+  ev.preventDefault();
+  
+  if (inputElement.value) {
+      
+    value = inputElement.value
+    const textElement = document.createElement("span");
+   
+
+    const btnElement = document.createElement("button");
+    btnElement.innerHTML = "Remover";
+
+    const liElement = document.createElement("li");
+    liElement.appendChild(textElement);
+    liElement.appendChild(btnElement);
+    
+
+    btnElement.onclick = () => {
+      ulElement.removeChild(liElement)  
+    }
+
+    ulElement.appendChild(liElement);
+    //função numeros primos
+    function numeroPrimo(value){
+            let primo = true
+            for(let i =2; i < value; i++){
+                if(value % i === 0){
+                    primo = false;
+                    break
+                }
+            }
+            if(primo == true && value > 0){ 
+                textElement.innerHTML = inputElement.value + " È um primo uhuuul!" ;
+            }
+            else{
+                textElement.innerHTML = inputElement.value + " Não é primo! que triste :("
+            }
+        }
+    }
+    numeroPrimo(value)
+};
